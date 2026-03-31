003f5b62a3aeacc6ad54860ca5d3e3c0bdfb7aad
Зайцев Данил Евгеньевич <zaytsev.de@gazprom-neft.ru>
2026-03-31 21:41:27 +0300

    Обновлен конфиг jest, добавлены моки для svg, исправлены ошибки; Добавлены тесты для Button и Sidebar; Обновлен конфиг линтера

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,24 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { Sidebar } from '@/widgets/Sidebar';
+import { renderWithTranslation } from '@/shared/lib/tests/renderWithTranslation/renderWithTranslation';
+
+describe('Sidebar', () => {
+    test('should render', () => {
+        renderWithTranslation(<Sidebar />);
+
+        const sidebar = screen.getByTestId('sidebar');
+
+        expect(sidebar).toBeInTheDocument();
+    });
+
+    test('should have collapsed class when toggled', () => {
+        renderWithTranslation(<Sidebar />);
+
+        const sidebar = screen.getByTestId('sidebar');
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+
+        fireEvent.click(toggleBtn);
+
+        expect(sidebar).toHaveClass('collapsed');
+    });
+});
