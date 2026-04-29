48d8e139142b29fe65afb8739530bcd9edea6845
Зайцев Данил Евгеньевич <zaytsev.de@gazprom-neft.ru>
2026-04-30 01:01:14 +0300

    Тест фикса

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,7 +1,5 @@
 import type { Preview } from '@storybook/react-webpack5';
 import '../../src/shared/assets/styles/storybook.scss';
-import { AppProviderDecorator } from '../../src/shared/config/storybook/app-provider-decorator/appProviderDecorator';
-import { RouterDecorator } from '../../src/shared/config/storybook/router-decorator/routerDecorator';
 
 const preview: Preview = {
     parameters: {
@@ -14,7 +12,7 @@ const preview: Preview = {
         },
     },
 
-    decorators: [AppProviderDecorator, RouterDecorator],
+    decorators: [],
 };
 
 export default preview;
